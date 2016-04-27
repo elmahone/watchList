4a0bc6f589295423041b4a94e190dcaf225455b0
elmahone <mahonen93@gmail.com>
2016-04-27 11:32:10 +0200

    mongo book test

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ var SampleApp = function () {
         });
     };
     // default to a 'localhost' configuration:
-    var connection_string = '127.0.0.1:27017/watchlist';
+    var connection_string = '127.0.0.1:27017/YOUR_APP_NAME';
     // if OPENSHIFT env variables are present, use the available connection info:
     if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
