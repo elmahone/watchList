b15e6d772ccf7f6978aff575f52a0aa382cfed58
elmahone <mahonen93@gmail.com>
2016-04-28 13:29:48 +0200

    search strated

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 'use strict';
 
 $(function () {
+    function displaySearchResults(results) {
+        var searchArr = results.Search;
+        var container = $('<div></div>');
+        var searchTitle = $('<h3></h3>');
+        if (results.Response == "True") {
+            for (var i = 0; i < searchArr.length; i++) {
+                console.log(searchArr[i]);
+                $('#error').hide();
+
+                $('#searchResults').append('<div id="' + searchArr[i].imdbID + '" class="result" onclick="displayData()"><h3>' + searchArr[i].Title + ' (' + searchArr[i].Year + ')</h3></div><hr>');
+
+
+            }
+            // displays error message if no results was found
+        } else {
+            $('#searchResults').hide();
+            $('#content').hide();
+            $('#error').find('.message').text('No results');
+            $('#error').show();
+        }
+    }
+
+
     function displayData(data) {
         if (data.Response == "True") {
             $('#error').hide();
@@ -23,17 +46,26 @@ $(function () {
     //makes an api call with given url    
     function apiCall(url) {
         $.get(url, function (response) {
-            displayData(response);
+            displaySearchResults(response);
         });
     }
-
+    function changePage(){
+        $('#back').on('click', function(event){
+            event.preventDefault();
+            
+        });
+        $('#forward').on('click', function(event){
+            event.preventDefault();
+            
+        });
+    }
     function initListeners() {
         $('form').on('submit', function (event) {
             event.preventDefault();
             const title = $('form').find('#title').val();
             const type = $('form').find('select').val();
             const year = $('form').find('#year').val();
-            const url = 'http://www.omdbapi.com/?t=' + title + '&y=' + year + '&type=' + type + '&tomatoes=true&plot=full';
+            const url = 'http://www.omdbapi.com/?s=' + title + '&y=' + year + '&type=' + type + '&tomatoes=true&plot=full';
             apiCall(url);
         });
     }
