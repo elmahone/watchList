e8aa348d66dd38b4a29b99487c7a841ef78feee3
elmahone <mahonen93@gmail.com>
2016-05-01 20:41:14 +0300

    asaas

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // default to a 'localhost' configuration:
-var connection_string = '127.0.0.1:27017/YOUR_APP_NAME';
+var connection_string = '127.0.0.1:27017/watchlist';
 // if OPENSHIFT env variables are present, use the available connection info:
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
