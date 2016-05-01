c8c5be9d04ddf382a2211491f6494ed365d3026b
elmahone <mahonen93@gmail.com>
2016-05-01 15:48:56 +0300

    test auth

---
+++ VARASTO/mongo.js
@@ -0,0 +1,12 @@
+//load the Client interface
+var MongoClient = require('mongodb').MongoClient;
+// default to a 'localhost' configuration:
+var connection_string = '127.0.0.1:27017/YOUR_APP_NAME';
+// if OPENSHIFT env variables are present, use the available connection info:
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
