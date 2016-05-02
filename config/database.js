d7c2bc7f45162efebae5c929ad5d905d8b4fe5af
elmahone <mahonen93@gmail.com>
2016-05-02 09:53:27 +0300

    adsasd

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // default to a 'localhost' configuration:
-var connection_string = '127.0.0.1:27017/watchlist';
+var connection_string = '127.10.252.2:27017/watchlist';
 // if OPENSHIFT env variables are present, use the available connection info:
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
@@ -9,6 +9,7 @@ if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         process.env.OPENSHIFT_APP_NAME;
 }
 console.log(connection_string);
+
 module.exports = {
 
     'url': connection_string
