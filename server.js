450c8cff5a7f87e50cc5de18e698ba69ec1e4989
elmahone <mahonen93@gmail.com>
2016-05-02 12:55:33 +0300

    adda

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.createConnection(configDB.url); // connect to our database
 
 require('./config/passport')(passport); // pass passport for configuration
 
-app.configure(function () {
+//app.configure(function () {
     // set up our express application
     app.use(morgan('dev')); // log every request to the console
     app.use(cookieParser()); // read cookies (needed for auth)
@@ -35,7 +35,7 @@ app.configure(function () {
     app.use(passport.initialize());
     app.use(passport.session()); // persistent login sessions
     app.use(flash()); // use connect-flash for flash messages stored in session
-});
+//});
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
