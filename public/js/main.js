1e62cab2c38f7d3daa3ef1c5ac2fd9b818a3d8d8
elmahone <mahonen93@gmail.com>
2016-05-03 13:29:08 +0300

    createuuser

--- public/js/main.js
+++ public/js/main.js
@@ -56,7 +56,7 @@ $(function () {
 
     //Displays data of a single result gotten from the api call
     function displayData(data) {
-        addToList("miika", data.imdbID);
+        addToList("miika", data);
         if (data.Response == "True") {
             $('#error').hide();
             $('#searchResults').hide();
