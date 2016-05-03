fe265d8f5fd651ac3b4f35050efe807731530a95
elmahone <mahonen93@gmail.com>
2016-05-03 22:15:57 +0300

    my list started

--- server.js
+++ server.js
@@ -190,7 +190,7 @@ var SampleApp = function () {
         };
         self.routes['/getUser'] = function (req, res) {
             MongoClient.connect('mongodb://' + connection_string, function (err, db) {
-                db.collection('user').find({username: req.username}).toArray(function (err, docs) {
+                db.collection('user').find({"username": req.username}).toArray(function (err, docs) {
                     res.send(docs);
                     db.close();
                 });
