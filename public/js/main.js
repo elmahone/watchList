7c6a66958c01a5a09aa68b4290466ea7d5a5059f
elmahone <mahonen93@gmail.com>
2016-05-07 19:14:25 +0300

    recent searches

--- public/js/main.js
+++ public/js/main.js
@@ -297,7 +297,7 @@ $(function () {
         $('.recentTitle').on('click', function (event) {
             event.preventDefault();
             var title = $(this).attr('id');
-            $('input:text').val(title);
+            $('form').find('#title').val(title);
             apiCallSearchTitle(title);
         });
     }
@@ -312,7 +312,7 @@ $(function () {
     function changePageListener() {
         $('.pageNum').on('click', function () {
             var page = $(this).attr("id");
-            var title = $('input:text').find('#title').val();
+            var title = $('form').find('#title').val();
             var type = $('form').find('select').val();
             var year = $('form').find('#year').val();
             apiCallSearchPage(title, type, year, page);
